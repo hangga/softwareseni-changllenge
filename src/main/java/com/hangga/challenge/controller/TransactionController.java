2de881d56b73919dbd2ef45bbeb78ea07d90b0a5
sayekti <sayekti@sebangsa.com>
2020-08-21 17:01:56 +0700

    put add data

--- src/main/java/com/hangga/challenge/controller/TransactionController.java
+++ src/main/java/com/hangga/challenge/controller/TransactionController.java
@@ -3,10 +3,7 @@ package com.hangga.challenge.controller;
 import com.hangga.challenge.model.Transaction;
 import com.hangga.challenge.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -25,6 +22,18 @@ public class TransactionController {
         return transactionRepository.findAll();
     }
 
+    @PutMapping("/transaction/{transaction_id}")
+    public Transaction add(@PathVariable("id") Long transaction_id,
+                           @RequestParam("amount") Double amount,
+                           @RequestParam("type") String type,
+                           @RequestParam("parent_id") Long parent_id){
+        Transaction transaction = new Transaction();
+        transaction.setId(transaction_id);
+        transaction.setType(type);
+        transaction.setAmount(amount);
+        return transactionRepository.save(transaction);
+    }
+
     @GetMapping("/transaction/{transaction_id}")
     public Optional<Transaction> findtransactionByid(@PathVariable("id") Long transaction_id) {
         return transactionRepository.findById(transaction_id);
@@ -39,8 +48,23 @@ public class TransactionController {
     @GetMapping("/type/{type}")
     public List<Transaction> getTransactionByType(EntityManager em, String type) {
         TypedQuery<Transaction> query = em.createQuery(
-                "SELECT id FROM transaction WHERE type = :name", Transaction.class);
-        return query.setParameter("name", type).getResultList();
+                "SELECT id FROM transaction WHERE type = :type", Transaction.class);
+        return query.setParameter("type", type).getResultList();
+    }
+
+
+    /**
+     * A sum of all transactions that are transitively linked by their parent_id to
+     * $transaction_id.
+     * @param em
+     * @param transaction_id
+     * @return
+     */
+    @GetMapping("/sum/{transaction_id}")
+    public Transaction getTransactionSum(EntityManager em, Long transaction_id) {
+        TypedQuery<Transaction> query = em.createQuery(
+                "SELECT sum(amount) as sum FROM transaction WHERE parent_id = :transaction_id", Transaction.class);
+        return query.setParameter("transaction_id", transaction_id).getSingleResult();
     }
 
 }
--- src/main/java/com/hangga/challenge/model/Transaction.java
+++ src/main/java/com/hangga/challenge/model/Transaction.java
@@ -6,12 +6,28 @@ import javax.persistence.*;
 @Table(name = "transaction")
 public class Transaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     Long id;
 
     @Column(name = "parent_id", nullable = false)
     Long parent_id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setParent_id(Long parent_id) {
+        this.parent_id = parent_id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 
     @Column(name = "type", nullable = false)
     String type;
